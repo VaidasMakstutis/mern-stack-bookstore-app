444f637a506ba21d922e91a68804b0b8b389b73a
Vaidas <vaidas.makstutis@gmail.com>
2024-07-09 15:51:49 +0300

    Node project, http route, mongoDB connection and mongoose

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://vaidasmakstutis:bookstoreapp@mern-stack-booksstore-a.w2w4wkz.mongodb.net/?retryWrites=true&w=majority&appName=MERN-stack-booksStore-app";
